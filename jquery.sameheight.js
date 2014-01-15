c144f4302eadfacc4ea4ffa945e5c472fe8a40b6
mrchimp <chimpytk@gmail.com>
2014-01-15 13:01:03 +0000

    add autoHeightCondition

--- jquery.sameheight.js
+++ jquery.sameheight.js
@@ -4,33 +4,39 @@
 
   $.fn.sameheight = function (options) {
     
-    elems = this;
+    function calc() {
+      var max = 0;
 
-    var settings = $.extend({
-      calcOnResize: true
+      elems.height('auto');
+
+      if (settings.autoHeightCondition()) {
+        elems.height('auto');
+        return true;
+      }
+
+      elems.each(function() {
+        if ($(this).height() > max)  {
+          max = $(this).height();
+        }
+      });
+
+      elems.height(max);
+    }
+
+    var elems = this,
+    settings = $.extend({
+      calcOnResize: true,
+      autoHeightCondition: function() {}
     }, options);
 
     calc();
 
     if (settings.calcOnResize) {
-      $(window).resize(calculate);
+      $(window).resize(calc);
     }
 
-    return elems;
+    return this;
   };
 
-  function calc() {
-    var max = 0;
-
-    elems.height('auto');
-
-    elems.each(function() {
-      if ($(this).height() > max)  {
-        max = $(this).height();
-      }
-    });
-
-    elems.height(max);
-  }
 
 }( jQuery ));
--- readme.md
+++ readme.md
@@ -9,3 +9,7 @@ options
 -------
 
 **calcOnResize** - Default: true. If true, heights will be recalculated when the window is resized.
+
+**autoHeightCondition** - A function. When this returns truthy, heights will be set to `defaultHeight`, otherwise heights will be matched.
+
+**defaultHeight** - String. Default: 'auto'. Height value to use when autoHeightCondition returns truthy.
